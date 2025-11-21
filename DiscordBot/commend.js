df7d802b56c9ad04e87584e593c76be075650729
Dipongkar Barmon <dipongkorbarman02@gmail.com>
2025-11-21 15:43:06 +0600

    hello

--- DiscordBot/commend.js
+++ DiscordBot/commend.js
@@ -1,18 +1,18 @@
-import { REST, Routes } from 'discord.js';
-const commands = [
-  {
-    name: 'create',
-    description: 'Create a new Short URL',
-  },
-];
-const rest = new REST({ version: '10' }).setToken( "MTQzODQ3ODQ5MTM1NTE4NTE1Mg.GP1ZW5.sUnUHxL-x8sUxT9sxuPu6Ak-5gZG896n8VzTvc");
+// import { REST, Routes } from 'discord.js';
+// const commands = [
+//   {
+//     name: 'create',
+//     description: 'Create a new Short URL',
+//   },
+// ];
+// const rest = new REST({ version: '10' }).setToken( "MTQzODQ3ODQ5MTM1NTE4NTE1Mg.GP1ZW5.sUnUHxL-x8sUxT9sxuPu6Ak-5gZG896n8VzTvc");
 
-try {
-  console.log('Started refreshing application (/) commands.');
+// try {
+//   console.log('Started refreshing application (/) commands.');
 
-  await rest.put(Routes.applicationCommands("1438478491355185152"), { body: commands });
+//   await rest.put(Routes.applicationCommands("1438478491355185152"), { body: commands });
 
-  console.log('Successfully reloaded application (/) commands.');
-} catch (error) {
-  console.error(error);
-}
+//   console.log('Successfully reloaded application (/) commands.');
+// } catch (error) {
+//   console.error(error);
+// }
--- DiscordBot/index.js
+++ DiscordBot/index.js
@@ -1,32 +1,32 @@
-import { Client, Events, GatewayIntentBits, messageLink } from 'discord.js';
+// import { Client, Events, GatewayIntentBits, messageLink } from 'discord.js';
 
-const client = new Client({ 
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ] 
- });
+// const client = new Client({ 
+//   intents: [
+//     GatewayIntentBits.Guilds,
+//     GatewayIntentBits.GuildMessages,
+//     GatewayIntentBits.MessageContent,
+//   ] 
+//  });
 
-client.on("messageCreate",(message)=>{
-    //console.log(message);
-    if(message.author.bot) return; 
-    if(message.content.startsWith('create')){
-       const url=message.content.split('create')[1];
-       return message.reply({
-        content:"Generating Short Id for "+url 
-       });
-    }
-    message.reply({
-      content:"Hi form Bot"
-    })
-})
+// client.on("messageCreate",(message)=>{
+//     //console.log(message);
+//     if(message.author.bot) return; 
+//     if(message.content.startsWith('create')){
+//        const url=message.content.split('create')[1];
+//        return message.reply({
+//         content:"Generating Short Id for "+url 
+//        });
+//     }
+//     message.reply({
+//       content:"Hi form Bot"
+//     })
+// })
 
-client.on("interactionCreate",(interaction)=>{
-  // console.log(interaction)
-  interaction.reply("Pong!!")
-})
+// client.on("interactionCreate",(interaction)=>{
+//   // console.log(interaction)
+//   interaction.reply("Pong!!")
+// })
 
-client.login(
-  "MTQzODQ3ODQ5MTM1NTE4NTE1Mg.GP1ZW5.sUnUHxL-x8sUxT9sxuPu6Ak-5gZG896n8VzTvc"
-)
+// client.login(
+//   "MTQzODQ3ODQ5MTM1NTE4NTE1Mg.GP1ZW5.sUnUHxL-x8sUxT9sxuPu6Ak-5gZG896n8VzTvc"
+// )
